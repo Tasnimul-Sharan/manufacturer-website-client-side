8d7beec245d50c19306bfa7442a9e7a0b7c35075
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-11-01 12:06:20 +0600

    updated header

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -42,10 +42,49 @@ const Header = () => {
   );
 
   return (
-    <div class="navbar bg-slate-800 text-white">
-      <div class="navbar-start">
-        <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost lg:hidden">
+    <div className="sticky top-0 z-40 backdrop-blur-md backdrop-hue-rotate-30 ">
+      <div class="navbar mx-auto bg-slate-800 text-white">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabindex="0"
+              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-800 rounded-box w-52"
+            >
+              {menuItems}
+            </ul>
+          </div>
+          <div className="flex justify-items-center mb-8">
+            <Link to="/" class="btn btn-ghost normal-case text-xl">
+              <img className="w-16" src={pic} alt="" />
+              <span className="m-3">Space Electronics</span>
+            </Link>
+          </div>
+        </div>
+        <div className="navbar navbar-end">
+          <div className=" hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+          </div>
+          <label
+            tabindex="1"
+            for="dashboard-sidebar"
+            class="btn btn-ghost lg:hidden"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               class="h-5 w-5"
@@ -61,44 +100,7 @@ const Header = () => {
               />
             </svg>
           </label>
-          <ul
-            tabindex="0"
-            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-800 rounded-box w-52"
-          >
-            {menuItems}
-          </ul>
         </div>
-        <div className="flex justify-items-center mb-8">
-          <Link to="/" class="btn btn-ghost normal-case text-xl">
-            <img className="w-16" src={pic} alt="" />
-            <span className="m-3">Space Electronics</span>
-          </Link>
-        </div>
-      </div>
-      <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal p-0">{menuItems}</ul>
-      </div>
-      <div className="navbar-end">
-        <label
-          tabindex="1"
-          for="dashboard-sidebar"
-          class="btn btn-ghost lg:hidden"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16"
-            />
-          </svg>
-        </label>
       </div>
     </div>
   );
